03427fb2d548ac50892297f6d834e3d42d092390
sebuszqo <sekn.kn@gmail.com>
2023-01-07 23:57:44 +0100

    Simple encrypted chat is done

--- lab-06-Bezpieczenstwo/encrypted-chat/main.py
+++ lab-06-Bezpieczenstwo/encrypted-chat/main.py
@@ -2,37 +2,38 @@ import socket
 import threading
 import rsa
 
-
-def sending_messages(client):
+        
+def sending_messages(c):
     while True:
         message = input("")
-        client.send(message.encode())
+        c.send(rsa.encrypt(message.encode(),public_partner))
         print(f"You've send {message}")
 
-def receiving_messages(client):
+def receiving_messages(c):
     while True:
-        message = input("")
-        print(f"You've send {client.recv(1024).decode()}")        
-
-try:
-    choise = int(input("What you want to do: *1* host or *2* connect: "))
-
-    match choise:
-        case 1:
-            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server.bind(("127.0.0.1", 9999))
-            server.listen()
-            client, _ = server.accept()
-        case 2:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect(("127.0.0.1", 9999))
-        case _:
-            exit()
-
-
-    threading.Thread(target=sending_messages, args=(client,)).start()
-    threading.Thread(target=receiving_messages, args=(client)).start()
-
-except ValueError:
-    print("Sorry, wrong choise!, try again")
+        print(f"You've received: {rsa.decrypt(c.recv(1024), private_key).decode()}")
     
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
+
+
+choise = input("What you want to do: *1* host or *2* connect: ")
+    
+if choise == "1":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", 9093))
+    server.listen()
+    client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+elif choise == "2":
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 9093))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    client.send(public_key.save_pkcs1("PEM"))
+else:
+    exit()
+
+
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
